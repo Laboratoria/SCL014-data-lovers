8dc1532c0e8b7a5854957b8dc51271ac10bcb2cd
acer <rverdufiguera@gmail.com>
2020-08-06 22:51:57 -0400

    si, deseo subir este cambio

--- src/data.js
+++ src/data.js
@@ -1,5 +1,6 @@
 // estas funciones son de ejemplo
 
 export const example = () => 'example';
+// en este archivo voy a crear funciones para filtrar y ordenar
 
 export const anotherExample = () => 'OMG';
--- src/main.js
+++ src/main.js
@@ -1,6 +1,22 @@
-import { example } from './data.js';
-// import data from './data/atletas/atletas.js';
-// import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
+import { example } from
+// aqui va todo lo del DOM para imprimir TODO  lo que queremos mostrar en el HTML
 
-console.log(example, data);
+// aqui recorro toda la data sin filtrar ni ordenar, solo mostrar informacion e imagen 
+// Importar las funciones que creamos en el data.js
+
+'./data.js';
+
+import data from './data/potter/potter.js';
+
+
+//console.log(example, data);
+
+const personajes = data;
+for (let i = 0; i <= personajes.length; i++) {
+    //console.log(personajes[i]);
+
+    //console.log(personajes[i].name)
+    //console.log(personajes[i].image)
+    const fotosPersonajes =
+
+}
