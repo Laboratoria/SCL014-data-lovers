2bdd9104c5c455780a51b57f307523d8e19b834c
acer <rverdufiguera@gmail.com>
2020-08-07 00:25:37 -0400

    si, listo

--- src/main.js
+++ src/main.js
@@ -8,14 +8,23 @@ import { example } from
 
 import data from './data/potter/potter.js';
 
+//console.log(example,data);
 
-//console.log(example, data);
+const mostrarPersonajes = () => {
+    // guardo la data en una variable y la llamo personajes
+    const personajes = data;
+    // realizo el recorrido con un for de la nueva variable para verificar que se guardo bien y semuestra la data
+    for (let i = 0; i <= personajes.length; i++) {
+        // con los console verifico que ya se puede recorrer segun las propiedades de la data
+        //console.log(personajes[i]);
+        //console.log(personajes[i].name)
+        //console.log(personajes[i].image)
+        // le doy funcionalidad al boton para mostrar los personajes en la pag
 
-const personajes = data;
-for (let i = 0; i <= personajes.length; i++) {
-    //console.log(personajes[i]);
-    //console.log(personajes[i].name)
-    //console.log(personajes[i].image)
-    const fotosPersonajes;
-
+        let boton = document.getElementById("btn-personajes");
+        boton.addEventListener("click", () => {
+            let imgPersonaje = document.getElementById("img").value;
+            imgPersonaje.innerHTML = 'Hola Bienvenido, elige un personaje:' + personajes[i].image;
+        })
+    }
 }
