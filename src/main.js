fdbe6a729e39436b53496c29c7ad09d359666c60
acer <rverdufiguera@gmail.com>
2020-08-09 13:31:17 -0400

    si

--- src/main.js
+++ src/main.js
@@ -25,7 +25,7 @@ const mostrarPersonajes = () => {
 
             namePersonaje.innerHTML +=
                 `<div class='imagenes'>
-                <img src = git${personajes[i].image }></div>
+                <img src = ${personajes[i].image }></div>
                 <div class='name'> <h1>${personajes[i].name}</h1></div>
                 <div class='name'> <h1>${personajes[i].dateOfBirth}`
 
