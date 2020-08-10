64e5bc40113eb60a80ee44d9ea89041a9792478d
acer <rverdufiguera@gmail.com>
2020-08-10 14:48:54 -0400

    ok

--- src/data.js
+++ src/data.js
@@ -1,6 +1,43 @@
 // estas funciones son de ejemplo
 
-export const example = () => 'example';
-// en este archivo voy a crear funciones para filtrar y ordenar
+//export const example = () => 'example';
 
-export const anotherExample = () => 'OMG';
+// export const anotherExample = () => 'OMG';
+
+// en este archivo voy a crear funciones para filtrar y ordenar
+// const house=   personajes.filtter.house
+//personajes.house.include(tipos de casas return con boton addverlistenner y tipos de casa(sus parametros))
+//const filterHOUSE = (DATA, TIPODEHOUSE) => {
+//const newaRRAY = DATA.filter(PERSONAJE => PERSONAJES.HOUSE.includes(TIPODEHOUSE));
+//return newArray;
+//};
+export const casaGryffindor = data.filter(function() {
+    return data.house === "Gryffindor";
+});
+// export const casaSlytherin = () => {
+//     let personajescasa2 = data.filter(data => (data.house === Slytherin));
+//     return personajescasa2;
+// }
+// export const casaHufflepuff = () => {
+//     let personajescasa3 = data.filter(data => (data.house === Hufflepuff));
+//     return personajescasa3;
+// }
+// export const casaRavenclaw = () => {
+
+//     let personajescasa4 = data.filter(data => (data.house === Ravenclaw));
+//     return personajescasa4;
+// }
+
+// const mostrarPersonajesEstudiantes = () => {
+
+// }
+// const mostrarPersonajesProfesores = () => {
+
+// }
+
+// const ordenarPerzonajesAsscendente = () => {
+
+// }
+// const ordenarPersonajesDescendente = () => {
+
+//}
--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-//import { example,data } from 
+import { casaGryffindor } from
 // aqui va todo lo del DOM para imprimir TODO  lo que queremos mostrar en el HTML
 // aqui recorro toda la data sin filtrar ni ordenar, solo mostrar informacion e imagen 
 // Importar las funciones que creamos en el data.js
@@ -8,28 +8,31 @@ import data from './data/potter/potter.js';
 // guardo la data en una variable y la llamo personajes
 //const personajes = data;
 //console.log(personajes)
-const personajes = data;
 const mostrarPersonajes = () => {
     // realizo el recorrido con un for de la nueva variable para verificar que se guardo bien y semuestra la data
-    for (let i = 0; i <= personajes.length; i++) {
-        console.log(personajes[i])
-            //con los console verifico que ya se puede recorrer segun las propiedades de la data
-            // console.log(personajes[i]);
-            //console.log(personajes[i].name
-            // console.log(personajes[i].image)
-            //le doy funcionalidad al boton para mostrar los personajes en la pag
+    for (let i = 0; i <= data.length; i++) {
+        //console.log(personajes[i])
+        //con los console verifico que ya se puede recorrer segun las propiedades de la data
+        // console.log(personajes[i]);
+        //console.log(personajes[i].name
+        // console.log(personajes[i].image)
+        //le doy funcionalidad al boton para mostrar los personajes en la pag
         let boton = document.getElementById("btn-personajes")
         boton.addEventListener("click", () => {
             let datosPersonajes = document.getElementById("name");
 
             datosPersonajes.innerHTML +=
                 `<div class='imagenes'>
-                <img src = ${personajes[i].image}></div>
-                <div class='name'> <h1>${personajes[i].name}</h1></div>
-                <div class='dateOfBirth'><h1>${personajes[i].dateOfBirth}</h1></div>
-                <div class= 'patronus'><h1>${personajes[i].patronus}</h1></div>
+                <img src = ${data[i].image}></div>
+                <div class='name'> <h1>${data[i].name}</h1></div>
+                <div class='dateOfBirth'><h1>${data[i].dateOfBirth}</h1></div>
+                <div class= 'patronus'><h1>${data[i].patronus}</h1></div>
                 <div> hola </div>`
         })
     }
 }
 mostrarPersonajes();
+
+let boton2 = document.getElementById("btn-filtro1")
+boton2.addEventListener("click", casaGryffindor);
+console.log(casaGryffindor);
