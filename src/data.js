2e78b95186671445110f8dc11ef63fda013f9bea
acer <rverdufiguera@gmail.com>
2020-08-12 14:54:50 -0400

    ok

--- src/data.js
+++ src/data.js
@@ -1,5 +1,15 @@
-// estas funciones son de ejemplo
+// estas funciones son de filtrar por casas
+//export const casaG = data.filter(mostrar => mostrar.house === 'Gryffindor');
+//console.log(casaG);
+// export const casaS = data.filter(data => data.house === 'Slytherin');
+// //console.log(casaS);
 
-export const example = () => 'example';
+// export const casaH = data.filter(data => data.house === 'Hufflepuff');
+// //console.log(casaH);
 
-export const anotherExample = () => 'OMG';
+// export const casaR = data.filter(data => data.house === 'Ravencla');
+// //console.log(casaR);
+
+
+// export const estudiantes = data.filter()
+// export const profesores = data.filter()
--- src/main.js
+++ src/main.js
@@ -1,39 +1,61 @@
-//import { example } from 
-// aqui va todo lo del DOM para imprimir TODO  lo que queremos mostrar en el HTML
+//import { casaG, casaS, casaH, casaR } from
 
+// aqui va todo lo del DOM para imprimir TODO  lo que queremos mostrar en el HTML
 // aqui recorro toda la data sin filtrar ni ordenar, solo mostrar informacion e imagen 
-// Importar las funciones que creamos en el data.js
+// // Importar las funciones que creamos en el data.js
 // './data.js';
-import data from './data/potter/potter.js';
-//console.log(example,data);
-// guardo la data en una variable y la llamo personajes
-//const personajes = data;
-//console.log(personajes)
-// const personajes = data;
+import data from './data/potter/potter.js'
+
+//FUNCION PARA MOSTRAR DATA
 const mostrarPersonajes = () => {
     // realizo el recorrido con un for de la nueva variable para verificar que se guardo bien y semuestra la data
     for (let i = 0; i <= data.length; i++) {
-        // console.log(data[i])
-            //con los console verifico que ya se puede recorrer segun las propiedades de la data
-            // console.log(personajes[i]);
-            //console.log(personajes[i].name
-            // console.log(personajes[i].image)
-            //le doy funcionalidad al boton para mostrar los personajes en la pag
         let boton = document.getElementById("btn-personajes")
         boton.addEventListener("click", () => {
             let namePersonaje = document.getElementById("name");
-
             namePersonaje.innerHTML +=
-                `<mein>
+                `<section>
                 <div class='imagenes'>
-                <img src =${data[i].image }></div>
+                <img src = ${data[i].image }></div>
                 <div class='name'> <h1>Nombre en la saga: ${data[i].name}</h1></div>
                 <div class='name'> <h1>Tipo de patronus: ${data[i].patronus}</div>
                 <div class='name'> <h1>Componente de Varita: ${data[i].wand}</div>
                 <div class='name'> <h1>Nombre del actor: ${data[i].actor}</div>
                 <div class='name'> <h1>Fecha de nacimiento: ${data[i].dateOfBirth}</div>
-                </mein>`
+                </section>`
         })
     }
 }
 mostrarPersonajes();
+
+const OrdenarData = () => {
+        for (let i = 0; i <= data.length; i++) {
+            let ordenarNombres = data[i].name;
+            ordenarNombres.sort(function(a, b) {
+                        if (a.name > b.name) {
+                            return 1;
+                        }
+                        if (a.name < b.name) {
+                            return -1;
+                        }
+                        console.log(ordenarNombres)
+                    }
+
+
+                    // document.getElementById('dataOrdenada').innerHTML = `
+                    //  < div class = "ordenar" >
+                    // < p > DataOrdenada < /p> <
+                    // input type = "button"
+                    // id = "ordenar_name"
+                    // value = "Alfabetico A-Z" > < br > < br >
+                    // <
+                    ///div> `
+
+
+                    // // DECLARO VARIABLES PARA LAS PAGINAS Y LE DOY EVENTOS 
+
+                    // let pag2 = document.getElementById("pag2")
+                    // let pag3 = document.getElementById("pag3")
+                    // let pag4 = document.getElementById("pag4")
+                    // let pag5 = document.getElementById("pag5")
+                    // let pag6 = document.getElementById("pag6")
